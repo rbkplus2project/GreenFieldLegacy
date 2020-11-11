485ff63b17d18664380cd7bdaa6e38bcc575453b
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-11-11 17:13:13 +0200

    try it

--- server/database/index.js
+++ server/database/index.js
@@ -11,11 +11,37 @@ const connect = mongoose.connect(dbURI,
   .catch(err => console.log(err));
 
 let userSchema = mongoose.Schema({
- 
+  username : {
+    type :String,
+    unique : true
+  },
+  password :String,
+  email : {
+    type : String,
+    unique : true
+  },
+  reservation : []   
 }, { timestamps: true });
 
 
+let payment = mongoose.Schema({
+  userid: {
+    type :Number,
+    unique : true
+  },
+  Goodthru :String,
+  pin : {
+    type : Number,
+    unique : true
+  },
+  arrayOffavouirt : []   
+}, { timestamps: true });
+
+
+
 let User = mongoose.model('User', userSchema);
+let payment = mongoose.model('payment', payment);
 
 
 module.exports.User = User;
+module.exports.payment = payment;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {User } = require("./database")
+const { User, payment } = require("./database")
 const cors = require("cors")
 
 
