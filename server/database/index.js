1f345015684a970388ace48b6af65bf924bc9683
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-11-11 12:10:00 +0200

    initalizing

---
+++ server/database/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const dbURI = 'mongodb+srv://asemOne:asem1234@cluster0.xqniz.mongodb.net/hotels?retryWrites=true&w=majority'
+const connect = mongoose.connect(dbURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+let userSchema = mongoose.Schema({
+ 
+}, { timestamps: true });
+
+
+let User = mongoose.model('User', userSchema);
+
+
+module.exports.User = User;
