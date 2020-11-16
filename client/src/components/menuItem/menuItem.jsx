af964fbd6adb8ecb6b0e92afe9542e3f44a84324
asemnouri <asem_nori@hotmail.com>
2020-11-17 00:08:04 +0200

    data remaining on the card

--- client/src/components/menuItem/menuItem.jsx
+++ client/src/components/menuItem/menuItem.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './menuItem.css'
-
+import {Link } from 'react-router-dom';
 
 
 class MenuItem extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            resulsArray:[]
+            resulsArray: []
         }
     }
- 
+
 
     render() {
-        const { currentUser, searchValue} =this.props
-      
+        const { currentUser, searchValue } = this.props
+
         return (
-            <div className={`${this.props.item.size} menu-item`} onClick={(e) => {e.preventDefault(); searchValue(this.props.item.city)}} >
-                <div className={`background-image ${this.props.item.city} `}>
-                </div>
+            <div className={`${this.props.item.size} menu-item`} onClick={(e) => { e.preventDefault(); searchValue(this.props.item.city) }} >
+                <Link to='/cardlist'>
+                    <div className={`background-image ${this.props.item.city} `}>
+                    </div>
+                </Link>
             </div>
         )
     }
