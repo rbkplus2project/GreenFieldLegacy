dd17f3d8b681d52db0e04f54ddc5613cdd23ff2f
asemnouri <asem_nori@hotmail.com>
2020-11-20 22:03:29 +0200

    images

--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -7,7 +7,7 @@ class HomePage extends React.Component {
   constructor(props) { 
     super(props)
     this.state = {
-      initailItems: [ { city: "rome",link:"https://cdn.citywonders.com/media/17764/rome-city.jpg?anchor=center&mode=crop&width=1200&height=600" }, { city: "Barcelona",link:"https://www.jetsetter.com/wp-content/uploads/sites/7/2018/04/Zrlvh0hw-1380x690.jpeg" },{ city: "losAngeles",link:"https://i.ytimg.com/vi/tvDH4JM_gME/maxresdefault.jpg" },{ city: 'paris',link:"https://cdn.pubble.io/resources/web/pubble/img/city_paris.png" },{ city: "singapore", link: 'https://i.pinimg.com/originals/51/9b/f5/519bf5f2b0ab2cbe84797f8b309baa16.jpg' }, { city: "kualalumpur", link: 'https://static.asiawebdirect.com/m/kl/portals/kuala-lumpur-ws/homepage/pagePropertiesOgImage/kuala-lumpur.jpg.jpg' }],
+      initailItems: [ { city: "rome",link:"https://cdn.citywonders.com/media/17764/rome-city.jpg?anchor=center&mode=crop&width=1200&height=600" }, { city: "Barcelona",link:"https://www.jetsetter.com/wp-content/uploads/sites/7/2018/04/Zrlvh0hw-1380x690.jpeg" },{ city: "losAngeles",link:"https://i.ytimg.com/vi/tvDH4JM_gME/maxresdefault.jpg" },{ city: 'paris',link:"https://cdn.hipwallpaper.com/i/82/73/QoZlmC.jpg" },{ city: "Dubai", link: 'https://images.hdqwalls.com/download/dubai-burj-khalifa-minimalist-d0-1280x1024.jpg' }, { city: "kualalumpur", link: 'https://wallpapercave.com/wp/wp2378918.jpg' }],
     }
   }
   render() {
