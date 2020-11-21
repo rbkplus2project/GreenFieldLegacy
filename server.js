ae347fb021897d781d05f78b0597181f8b539b2f
asemnouri <asem_nori@hotmail.com>
2020-11-21 12:09:28 +0200

    heroku

--- server.js
+++ server.js
@@ -16,15 +16,13 @@ app.use('/fav', require('./server/routes/favourates'));
 app.use('/reservation', require('./server/routes/resevation'));  
 app.use("/payment",require("./server/routes/payment"))
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
 }
-
 let port =  process.env.PORT || 5000;
 
 app.listen(port, function () {
