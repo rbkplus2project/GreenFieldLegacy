d48797eaefa7f1ff13f038516948635e3d6b49d8
hamdalla2 <hamdalla2@yahoo.com>
2020-11-23 17:32:41 +0200

    adding map

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const dbURI = 'mongodb+srv://gamesio:fwOQ12Ol9t6CiHnb@gamesio.h6rcl.mongodb.net/gamesio?retryWrites=true&w=majority'
+const dbURI = process.env.MONGODB || 'mongodb+srv://gamesio:fwOQ12Ol9t6CiHnb@gamesio.h6rcl.mongodb.net/gamesio?retryWrites=true&w=majority'
 var connect = () => {
   mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true, 'useFindAndModify': false })
     .then(() => console.log('MongoDB Connected...'))
