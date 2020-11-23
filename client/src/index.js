e16c1b029e9ddb07287759e5041de13244d3aec1
Yasir Tobbileh <yasur96@gmail.com>
2020-11-23 15:53:00 +0200

    Adding Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
